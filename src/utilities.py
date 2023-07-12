cc2e63c845c050269a3c4c9e193fac91b5bf4cd0
MohdFarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2023-07-12 14:52:44 +0300

    Add equationParser function

--- src/utilities.py
+++ src/utilities.py
@@ -1,5 +1,8 @@
 # pylint: disable=C0103,W0105,E0602
 
+import numpy as np
+from math import *
+
 COLOR1 = "#222831"
 COLOR2 = "#393E46"
 COLOR3 = "#00ADB5"
@@ -9,7 +12,18 @@ COLOR5 = "#ffffff"
 """Functions
 """
 
-# Take min and max values of x from the user.
-def functionTranslator(equation: str, min:float, max:float):
+def functionTranslator(equation: str, min:float, max:float) -> np.ndarray:
+    try:
+        eval(equation, None, {'x': 0})
+    except Exception as e:
+        raise e
+    
     # The following operators must be supported: + - / * ^.
-    pass
+    equation = equation.replace("^","**") 
+    x_axis = np.arange(min,max,0.01)
+    y_axis = list()
+    for x in x_axis:
+        result = eval(equation, None, {'x': x})
+        y_axis.append(result)
+
+    return x_axis, y_axis
--- test/test.py
+++ test/test.py
@@ -1,68 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import *
 
-# Get mean of image
-def getMean(image:np.ndarray) -> float:
-    """ Get mean of image
-
-    Args:
-        image (np.ndarray): image to get mean of
-
-    Returns:
-        float: mean of image
-    """
+def functionTranslator(equation: str, min:float, max:float) -> np.ndarray:
+    try:
+        eval(equation, None, {'x': 0})
+    except Exception as e:
+        raise e
     
-    # Flatten image
-    image = image.flatten()
-    # Get sum of all numbers in image
-    sumOfNumbers = image.sum()
-    # Get mean of image
-    mean = sumOfNumbers / (image.shape[0])
-    
-    return mean
-    
-# Get median of image
-def getMedian(image:np.ndarray) -> float:
-    """ Get median of image
+    # The following operators must be supported: + - / * ^.
+    equation = equation.replace("^","**") 
+    x_axis = np.arange(min,max,0.01)
+    y_axis = list()
+    for x in x_axis:
+        result = eval(equation, None, {'x': x})
+        y_axis.append(result)
 
-    Args:
-        image (np.ndarray): image to get median of
+    return x_axis, y_axis
 
-    Returns:
-        float: median of image
-    """
-    
-    # Flatten image
-    image = image.flatten()
-    # Sort image
-    image.sort()
-    # Get median of image
-    median = image[len(image)//2]
-    
-    return median
 
-# Get variance of image
-def getVariance(image:np.ndarray) -> float:
-    """ Get variance of image
-
-    Args:
-        image (np.ndarray): image to get variance of
-
-    Returns:
-        float: variance of image
-    """
-    
-    # Flatten image
-    image = image.flatten()
-    # Get mean of image
-    mean = getMean(image)
-    # Get variance of image
-    variance = ((image - mean)**2).sum() / (image.shape[0])
-    
-    return variance
-
-image = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
-print("Mean: ", getMean(image))
-print("Median: ", getMedian(image))
-print("Variance: ", getVariance(image))
+try:
+    eq = "e**x"    
+    min,max=-10,50
+    x,y = functionTranslator(eq,min,max)
+    plt.plot(x,y)
+    plt.show()
+except Exception as e:
+    print(e)    
